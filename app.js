60be0757656639d1a2f887563c297c2e0616e047
Edgar Santana <santanaprogrammer@gmail.com>
2021-08-05 23:41:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var turns=["#", "#", "#", "#", "#", "#", "#", "#", "#",];
 var computerTurn="0";
 var computerOn = false;
 var count=0;
+
+document.getElementById('turnX').addEventListener('click', () => {
+    turn = "O";
+    computerTurn= "X";
+    document.getElementById('turnX').remove('btn-primary');
+    document.getElementById('turnO').add('btn-primary');
+    
+})
