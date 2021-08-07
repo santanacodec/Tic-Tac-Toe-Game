05289e2fd471b057bff7e03307e7a24e52b94975
Edgar Santana <santanaprogrammer@gmail.com>
2021-08-06 22:45:09 -0400

    Update app.js
    fixed bug.

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var count=0;
 document.getElementById('turnX').addEventListener('click', () => {
     turn = "O";
     computerTurn= "X";
-    document.getElementById('turnX').remove('btn-primary');
-    document.getElementById('turnO').add('btn-primary');
+    document.getElementById('turnX').classList.remove('btn-primary');
+    document.getElementById('turnO').classList.add('btn-primary');
 });
 
 document.getElementById('turnO').addEventListener('click', () => {
     turn = "X";
     computerTurn= "O";
-    document.getElementById('turnO').remove('btn-primary');
-    document.getElementById('turnX').add('btn-primary');
+    document.getElementById('turnO').classList.remove('btn-primary');
+    document.getElementById('turnX').classList.add('btn-primary');
 });
 
